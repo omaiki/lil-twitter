d7e174a53c83ed6563dce8463e11f926e114ea61
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-05 14:35:44 -0700

    Fix bug in user controller post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,15 @@ end
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
-    session[:id] = user.id
-    redirect "users/#{@user.id}"
+    session[:id] = @user.id
+    redirect "/users/#{@user.id}"
   else
-    @errors = @users.errors.full_messages
+    @errors = @user.errors.full_messages
     erb :"users/new"
   end
 end
 
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :"/users/show"
+end
