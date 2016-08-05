2f016bee07f5509f4d35bc21d23d230dce61630e
parthahirgit config --global user.email parthahir@umail.ucsb.edugit config --global user.name “parthahir <parthahir@umail.ucsb.edu>
2016-08-05 11:48:17 -0700

    Add restful routes to sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+# Sessions NEW
+get '/sessions/new' do
+ erb :'sessions/new'
+end
+
+# Sessions CREATE
+post '/sessions' do
+ @user = User.find_by(username: params[:user][:username])
+ if @user
+   if @user.authenticate(params[:user][:password])
+     session[:id] = @user.id
+     redirect "/users/#{@user.id}"
+   else
+   	 @error = "Invalid login information. Please try again."
+     redirect 'sessions/new'
+   end
+ else
+   @error = "You are not in the user database. Please sign up." 
+   redirect 'users/new'
+ end
+end
+
+# Sessions DELETE
+delete '/sessions/:id' do
+ session[:id] = nil
+ redirect "/"
+end
